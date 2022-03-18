225926b4dc8435d22aa7b13f392118ed5f441443
Minh Nam <duongminhnam99@gmail.com>
2022-03-19 00:17:06 +0700

    [Update] NFT detail page

--- client/pages/nfts/[nftId].js
+++ client/pages/nfts/[nftId].js
@@ -1,10 +1,85 @@
-import React from 'react'
+import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import { useRouter } from 'next/router'
+import { useEffect, useMemo, useState } from 'react'
 import Header from '../../components/Header'
+import GeneralDetails from '../../components/nft/GeneralDetails'
+import ItemActivity from '../../components/nft/ItemActivity'
+import NFTImage from '../../components/nft/NFTImage'
+
+const style = {
+  wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
+  container: `container p-6`,
+  topContent: `flex`,
+  nftImgContainer: `flex-1 mr-4`,
+  detailsContainer: `flex-[2] ml-4`,
+}
 
 const Nft = () => {
-    return (
-        <Header />
+  const { provider } = useWeb3()
+  const [selectedNft, setSelectedNft] = useState()
+  const [listings, setListings] = useState([])
+  const router = useRouter()
+
+  const nftModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/7gh6tiPwjOeiC6anaB_OoIOJ0kYjDmJS'
     )
+    return sdk.getNFTModule('0xEc8D49123962446b4214bB56ee1a71dBF5F30397')
+  }, [provider])
+
+  useEffect(() => {
+    if (!nftModule) return
+    ;(async () => {
+      const nfts = await nftModule.getAll()
+
+      const selectedNftItem = nfts.find((nft) => nft.id === router.query.nftId)
+
+      setSelectedNft(selectedNftItem)
+    })()
+  }, [nftModule])
+
+  const marketPlaceModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/7gh6tiPwjOeiC6anaB_OoIOJ0kYjDmJS'
+    )
+
+    return sdk.getMarketplaceModule(
+      '0xbf68C5560Fcef018d4441d9a2E47c3C72b95364c'
+    )
+  }, [provider])
+
+  useEffect(() => {
+    if (!marketPlaceModule) return
+    ;(async () => {
+      setListings(await marketPlaceModule.getAllListings())
+    })()
+  }, [marketPlaceModule])
+
+  return (
+    <div>
+      <Header />
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <div className={style.topContent}>
+            <div className={style.nftImgContainer}>
+              <NFTImage selectedNft={selectedNft} />
+            </div>
+            <div className={style.detailsContainer}>
+              <GeneralDetails selectedNft={selectedNft} />
+            </div>
+          </div>
+          <ItemActivity />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Nft
---
+++ client/static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 2,
+    from: '0xHdmn',
+    to: '0xHarryDuong',
+    date: '4 months ago',
+  },
+  {
+    price: 1,
+    from: '0xHdmn',
+    to: '0xHarryDuong',
+    date: '4 months ago',
+  },
+  {
+    price: 6,
+    from: '0xHdmn',
+    to: '0xHarryDuong',
+    date: '4 months ago',
+  },
+  {
+    price: 5,
+    from: '0xHdmn',
+    to: '0xHarryDuong',
+    date: '4 months ago',
+  },
+]
